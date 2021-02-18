65721773de59083bf9fe7de941992af9a434b101
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-02-18 15:11:55 -0600

    ADD: indice para exportacion de componentes

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
-export { default as Header } from './Header/Header'
+export { default as Head } from './Head/Head'
+export { default as Nav } from './Nav/Nav'
+export { default as LimiteCombos } from './LimiteCombos/LimiteCombos'
