072b8426ddcf920218b8e1ef20237c935ec305c3
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-02-23 16:37:41 -0600

    ADD: se termina la vista para mostrar combos

--- src/components/LimiteCombos/LimiteCombos.jsx
+++ src/components/LimiteCombos/LimiteCombos.jsx
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LimiteCombos.css';
-import { Col, Row } from 'reactstrap';
+import addImage from '../../assets/images/header/add.png';
 
-import CombosTable from './CombosTable/CombosTable'
+import { Col, Row } from 'reactstrap';
+import CombosTable from './CombosTable/CombosTable';
+import ModalCreateLimit from './ModalCreateLimit/ModalCreateLimit'
 
 const LimiteCombos = () => {
+	const [modal, setModal] = useState(false);
+
+	const toggle = () => setModal(!modal);
+
 	return (
-        <>
-		<Row>
-			<Col xs='10' sm='10'>
-				<h3 className='encabezado text-center mt-4'>
-					Límite de Combos
-				</h3>
-			</Col>
+		<>
+			<Row>
+				<Col xs='10' sm='10'>
+					<h3 className='encabezado text-center mt-4'>
+						Límite de Combos
+					</h3>
+				</Col>
 
-			<Col xs='2' sm='2' className='d-flex'>
-				<button className='add-combo mt-3'>
-					<img
-						src='images/header/add.png'
-						alt='Add logo'
-						className='py-2 mr-3'
-					/>
-				</button>
-				<h3 className='encabezado text-center pt-4'>Crear un Combo</h3>
-			</Col>
-		</Row>
+				<Col xs='2' sm='2' className='d-flex'>
+					<button className='add-combo mt-3' onClick={toggle}>
+						<img
+							src={addImage}
+							alt='Add logo'
+							className='py-2 mr-3'
+						/>
+					</button>
+					<h3 className='encabezado text-center pt-4'>
+						Crear un Combo
+					</h3>
+				</Col>
+			</Row>
 
-        <Row>
-            <Col sm='12' md='12'>
-                <CombosTable />
-            </Col>
+			<Row>
+				<Col sm='12' md='12'>
+					<CombosTable />
+				</Col>
+			</Row>
 
-        </Row>
-        </>
+			<ModalCreateLimit
+				modal={modal}
+				toggle={toggle}
+			/>
+		</>
 	);
 };
 
