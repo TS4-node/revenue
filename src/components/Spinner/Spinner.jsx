45c2196ff29409cfcd74965559d7de8c1a384671
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-03-01 15:01:01 -0600

    FIX: desisntale la libreria de spinner y cree uno con html y css

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import Loader from "react-loader-spinner";
+import "./Spinner.css";
 
 const Spinner = () => {
-    return (
-        <Loader
-        type="Puff"
-        color="#1890ff"
-        height={100}
-        width={100}
-        timeout={1000} //3 secs
-      />
-    )
-}
+	return (
+		<div className="my-5 py-5 mx-auto">
+			<div class="sk-circle">
+				<div class="sk-circle1 sk-child"></div>
+				<div class="sk-circle2 sk-child"></div>
+				<div class="sk-circle3 sk-child"></div>
+				<div class="sk-circle4 sk-child"></div>
+				<div class="sk-circle5 sk-child"></div>
+				<div class="sk-circle6 sk-child"></div>
+				<div class="sk-circle7 sk-child"></div>
+				<div class="sk-circle8 sk-child"></div>
+				<div class="sk-circle9 sk-child"></div>
+				<div class="sk-circle10 sk-child"></div>
+				<div class="sk-circle11 sk-child"></div>
+				<div class="sk-circle12 sk-child"></div>
+                <p style={{color:'#1890ff', fontSize:'10px', paddingTop:'2rem', fontWeight:'bold'}} className='pl-3'>Cargando</p>
+			</div>
+		</div>
+	);
+};
 
-export default Spinner
+export default Spinner;
