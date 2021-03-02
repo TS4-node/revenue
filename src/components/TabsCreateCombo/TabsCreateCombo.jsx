6d286543359bc2045c7ae007b699e69979ea4be6
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-03-01 19:40:51 -0600

    FIX: refactorice el codigo del componente para hacerlo mas facil de leer y no tenga tanta logica dentro de el

--- src/components/TabsCreateCombo/TabsCreateCombo.jsx
+++ src/components/TabsCreateCombo/TabsCreateCombo.jsx
@@ -1,31 +1,23 @@
 import React, { useState } from "react";
+import "./TabsCreateCombo.css";
 import SwipeableViews from "react-swipeable-views";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Box from "@material-ui/core/Box";
+import { AppBar, Tabs, Tab } from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
-import "./TabsCreateCombo.css";
-import { ComboData } from '../index'
+import { TabPanel , ComboData, ExclusionsAndInclusions } from "../index";
+import PopoverExclusionsAndInclusions from './PopoverExclusionsAndInclusions/PopoverExclusionsAndInclusions';
 
 
-
-function TabPanel(props) {
-	const { children, value, index, ...other } = props;
-
-	return (
-		<div
-			role="tabpanel"
-			hidden={value !== index}
-			id={`full-width-tabpanel-${index}`}
-			aria-labelledby={`full-width-tab-${index}`}
-			{...other}
-		>
-			{value === index && <Box p={3}>{children}</Box>}
-		</div>
-	);
-}
+const useStyles = makeStyles((theme) => ({
+	root: {
+		backgroundColor: theme.palette.background.paper,
+		width: 1200,
+	},
+	Tab: {
+		flexDirection: 'row-reverse'
+	}
+}));
 
 function a11yProps(index) {
 	return {
@@ -34,17 +26,14 @@ function a11yProps(index) {
 	};
 }
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		backgroundColor: theme.palette.background.paper,
-		width: 1200,
-	},
-}));
 
-export default function FullWidthTabs() {
-
+const TabsCreateCombo = () => {
+	/*
+	 * For de Tabs in nav
+	 */
 	const classes = useStyles();
 	const theme = useTheme();
+
 	const [value, setValue] = useState(0);
 
 	const handleChange = (event, newValue) => {
@@ -54,6 +43,19 @@ export default function FullWidthTabs() {
 	const handleChangeIndex = (index) => {
 		setValue(index);
 	};
+
+	/*
+	 * for Tab & PopoverExclusionsAndInclusions
+	 */
+
+	const [anchorEl, setAnchorEL] = useState(null);
+	const [view, setView] = useState(0);
+
+	const handleClick = (event) => {
+		event.stopPropagation();
+		setAnchorEL(event.currentTarget);
+	};
+
 
 	return (
 		<div className={classes.root}>
@@ -67,32 +69,42 @@ export default function FullWidthTabs() {
 					style={{ height: "1rem" }}
 				>
 					<Tab label="DATOS DEL COMBO" {...a11yProps(0)} />
-					<Tab label="EXCLUSIONES E INCLUSIONES" {...a11yProps(1)} />
+					<Tab
+						label={"Exclusiones e Inclusiones"}
+						icon={<ArrowDropDownIcon onClick={handleClick} />}
+					/>
 					<Tab label="BUSQUEDA DE MATERIALES" {...a11yProps(2)} />
 					<Tab label="RESUMEN DEL COMBO" {...a11yProps(3)} />
 				</Tabs>
+
+				<PopoverExclusionsAndInclusions anchorEl={anchorEl} setAnchorEL={setAnchorEL} setView={setView}/>
+
 			</AppBar>
+
 			<SwipeableViews
-				axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+				axis='x'
 				index={value}
 				onChangeIndex={handleChangeIndex}
 			>
 				<TabPanel value={value} index={0} dir={theme.direction}>
-					<ComboData setValue={setValue}/>
+					<ComboData setValue={setValue} />
 				</TabPanel>
 
 				<TabPanel value={value} index={1} dir={theme.direction}>
-					
+					<ExclusionsAndInclusions view={view} setView={setView}/>
 				</TabPanel>
 
 				<TabPanel value={value} index={2} dir={theme.direction}>
-					
+
 				</TabPanel>
 
 				<TabPanel value={value} index={3} dir={theme.direction}>
-					
+
 				</TabPanel>
+
 			</SwipeableViews>
 		</div>
 	);
 }
+
+export default TabsCreateCombo;
