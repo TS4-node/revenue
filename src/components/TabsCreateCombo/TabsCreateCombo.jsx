ebb28df3f30afaf52fe0c4b165eb7a52fce4ab99
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-03-03 12:08:52 -0600

    FIX: agregue redux en el componente padre, para mandar la data al hijo

--- src/components/TabsCreateCombo/TabsCreateCombo.jsx
+++ src/components/TabsCreateCombo/TabsCreateCombo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./TabsCreateCombo.css";
 import SwipeableViews from "react-swipeable-views";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
@@ -8,6 +8,8 @@ import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import { TabPanel , ComboData, ExclusionsAndInclusions } from "../index";
 import PopoverExclusionsAndInclusions from './PopoverExclusionsAndInclusions/PopoverExclusionsAndInclusions';
 
+import { useDispatch } from 'react-redux';
+import { getAllGeneralDirectorateSalesAction } from '../../redux/actions/generalDirectorateSalesActions'
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -28,6 +30,18 @@ function a11yProps(index) {
 
 
 const TabsCreateCombo = () => {
+
+	/*
+	 * Redux
+	*/
+
+	const dispatch = useDispatch();
+	 useEffect(() => {
+		 const getGeneralDirectorateSales = () => dispatch( getAllGeneralDirectorateSalesAction() );
+		 getGeneralDirectorateSales();
+	 }, [])
+
+
 	/*
 	 * For de Tabs in nav
 	 */
@@ -85,6 +99,7 @@ const TabsCreateCombo = () => {
 				axis='x'
 				index={value}
 				onChangeIndex={handleChangeIndex}
+				style={{paddingTop:'0rem'}}
 			>
 				<TabPanel value={value} index={0} dir={theme.direction}>
 					<ComboData setValue={setValue} />
