b15cd1adcea795d4e29b8dabe3271ec155f1aaad
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-03-03 12:11:17 -0600

    ADD: actualizacion de index para componentes, types y reducers

--- src/components/ExclusionsAndInclusions/index.js
+++ src/components/ExclusionsAndInclusions/index.js
@@ -1,4 +1,4 @@
-export { default as RegionalSalesDepartment } from './RegionalSalesDepartment/RegionalSalesDepartment';
+export { default as GeneralDirectorateSales } from './GeneralDirectorateSales/GeneralDirectorateSales';
 export { default as SalesOrganization } from './SalesOrganization/SalesOrganization';
 export { default as SalesOfice } from './SalesOfice/SalesOfice';
 export { default as Customers } from './Customers/Customers';
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import limitOfCombosReducer from './limitOfCombosReducer'
 import comboDataReducer from './comboDataReducer';
+import generalDirectorateSalesReducer from './generalDirectorateSalesReducer';
+import exclusiosnAndInclusionsReducer from './exclusiosnAndInclusionsReducer'
 
 export default combineReducers({
     limitOfCombos: limitOfCombosReducer,
-    comboData: comboDataReducer
+    comboData: comboDataReducer,
+    generalDirectorateSales: generalDirectorateSalesReducer,
+    exclusionAndInclusion: exclusiosnAndInclusionsReducer
 });
