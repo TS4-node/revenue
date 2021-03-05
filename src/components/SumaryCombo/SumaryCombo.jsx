0898075d6d9a7e01a62e49f12b3eabd177a56f69
Omar Jiménez <omarjimdiz-coder@github.com>
2021-03-05 00:52:23 -0600

    Se hizo componente BoxOne para el componente SumaryCombo.

--- src/components/SumaryCombo/SumaryCombo.jsx
+++ src/components/SumaryCombo/SumaryCombo.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import BoxOne from './BoxOne'
+
+import './SumaryCombo.css'
 
 const SumaryCombo = () => {
     return (
-        <div>
-            <h1>Resumen del Combo</h1>
+        <div className='pt-5 container'>
+            <h5><strong>COMBO 2021</strong></h5>
+            <h6 className='pt-2'>Combos MEGA</h6>
+            <BoxOne />
         </div>
     )
 }
