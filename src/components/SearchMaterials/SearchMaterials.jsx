a2d5d941e201bc844109f8187eb5d938544b5c98
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-03-19 15:32:24 -0600

    ADD: componente padre para busqueda de materiales "producto"

---
+++ src/components/SearchMaterials/SearchMaterials.jsx
@@ -0,0 +1,80 @@
+/*	COMBOS HEROKU
+ *  March 2021
+ *
+ *  Author: Alejandro Montes de Oca TS4
+ *  Description: handler for content capture in the tabs for exclusions and inclusions
+ *  =========================================================================
+ *  Information about changes:
+ *
+ *  No.         Date.        Author.      		Description.
+ *
+ *
+ */
+import React, { useState } from 'react';
+import { Container, Row, Col, Alert } from 'reactstrap';
+
+import Products from './Products/Products';
+import Quota from './Quota/Quota';
+import { useSelector } from 'react-redux';
+
+const SearchMaterials = ({ view, setView, setValue }) => {
+	/*	Redux	*/
+
+	const products = useSelector(state => state.materials.GET_products);
+
+	/*	State Local	*/
+	const [buttonProductsActive, setButtonProductsActive] = useState(false);
+	const [buttonQuotaActive, setButtonQuotaActive] = useState(false);
+
+	const handleButtonActiveProducts = () => {
+		setButtonProductsActive(true);
+		setButtonQuotaActive(false);
+	};
+	const handleButtonActiveQuota = () => {
+		setButtonQuotaActive(true);
+		setButtonProductsActive(false);
+	};
+
+	return (
+		<Container className='py-0 my-0'>
+			<Container className='px-5 mt-3'>
+				<Row className='text-center px-5'>
+					<Col sm='6' md='6' className='pl-4'>
+						<div
+							className={buttonProductsActive ? 'pseudo-btn pseudo-btn-active' : 'pseudo-btn'}
+							onClick={handleButtonActiveProducts}>
+							<p className='m-0'>Producto</p>
+						</div>
+					</Col>
+
+					<Col sm='6' md='6' className='pr-4'>
+						<div
+							className={buttonQuotaActive ? 'pseudo-btn pseudo-btn-active' : 'pseudo-btn'}
+							onClick={handleButtonActiveQuota}>
+							<p className='m-0'>Cupo</p>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+
+			<Container className='my-3'>
+				{!buttonProductsActive && !buttonQuotaActive && (
+					<Alert color='light' className='text-center mt-5 pt-5'>
+						Selecciona tus materiales
+					</Alert>
+				)}
+				{buttonProductsActive && (
+					<Products
+						setView={setView}
+						setValue={setValue}
+						products={products}
+						handleButtonActiveQuota={handleButtonActiveQuota}
+					/>
+				)}
+				{buttonQuotaActive && <Quota setView={setView} setValue={setValue} />}
+			</Container>
+		</Container>
+	);
+};
+
+export default SearchMaterials;
