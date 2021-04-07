8526b4f27d45f023129c4ac083086066680d6df3
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-04-07 12:53:52 -0500

    ADD: se agrega RevenueRouter

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,2 +1,3 @@
 export { default as CombosGeneratorRouter } from './CombosGeneratorRouter'
 export { default as AppRouter } from './AppRouter'
+export { default as RevenueRouter } from './RevenueRouter'
